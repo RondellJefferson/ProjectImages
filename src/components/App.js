8462e6a5dfc14baaef6e3e959f1218d5ab58b5ad
Rondell Jefferson <rondell804@gmail.com>
2021-12-11 23:52:38 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles.css";
+import React from "react";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <img src="" alt="Punk img" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
